3536168f30cbc583f82f11d963798b1d843fa537
Priates of Zimsey <realcoder2023@gmail.com>
2025-10-24 14:40:03 -0400

    added map types

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,29 @@ import fs from 'fs';
 
 interface Typed {
     getColorInHex(): void;
-
     convertColorBaseNumber(): void;
-
     createFile(fileName: string, data: string): void
 }
+
+interface UserPerson {
+    name: string,
+    isAdmin: true
+    getPerms(): void;
+}
+
+type ReadType= {
+    readonly [N in keyof  UserPerson]: UserPerson[N];
+}
+
+const person: ReadType = {
+    name: `Welcome to Hex Conversion App`,
+    isAdmin: true,
+
+    getPerms():void {
+        console.log("Title: " + this.name)
+    }
+}
+
 
 type Type = string
 type Conversion = Typed | Type
@@ -15,37 +33,43 @@ let colorR = Math.floor(Math.random() * 255);
 let colorG = Math.floor(Math.random() * 255);
 let colorB = Math.floor(Math.random() * 255);
 
-const conversion:Conversion = {
+if (person.isAdmin) {
 
-    getColorInHex() {
-        console.log("#" +
-            colorR.toString(15) + colorG.toString(16) + colorB.toString(16));
-    },
+    person.getPerms();
 
-    convertColorBaseNumber() {
+    const conversion: Conversion = {
 
-        console.log(`R: ${colorR} G: ${colorG} B: ${colorB}`)
-    },
+        getColorInHex() {
+            console.log("#" +
+                colorR.toString(15) + colorG.toString(16) + colorB.toString(16));
+        },
 
-    createFile(fileName: string, data: string) {
+        convertColorBaseNumber() {
 
-        fs.writeFileSync(fileName, data.toString());
+            console.log(`R: ${colorR} G: ${colorG} B: ${colorB}`)
+        },
 
+        createFile(fileName: string, data: string) {
+
+            fs.writeFileSync(fileName, data.toString());
+
+        }
+    }
+
+    const fileName = "colors.color.col"
+    let color = {
+        r: colorR,
+        g: colorG,
+        b: colorB
+    }
+
+    let message = `R: ${color.r} G: ${color.g} B: ${color.b} `
+
+    if (typeof conversion == "object") {
+
+        conversion.createFile(fileName, message.toString());
+        conversion.getColorInHex()
+        conversion.convertColorBaseNumber()
     }
 }
 
-const fileName =  "colors.color.col"
-let color = {
-    r: colorR,
-    g: colorG,
-    b: colorB
-}
-
-let message = `R: ${color.r} G: ${color.g} B: ${color.b} `
-
-if (typeof conversion == "object") {
-
-    conversion.createFile(fileName, message.toString());
-    conversion.getColorInHex()
-    conversion.convertColorBaseNumber()
-}
